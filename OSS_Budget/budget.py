215d6997d8df33db07e2f31a5660e55fdf17d2c3
Koen <gjyj0223@gmail.com>
2025-06-10 06:36:38 -0800

    [기능추가] 지출 삭제 기능 구현

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -11,6 +11,24 @@ class Budget:
         self.expenses.append(expense)
         print("지출이 추가되었습니다.\n")
 
+    def delete_expense(self):
+        if not self.expenses:
+            print("삭제할 지출 내역이 없습니다.\n")
+            return
+        self.list_expenses()
+        try:
+            choice = int(input("삭제할 지출의 번호를 입력하세요: "))
+            if 0 < choice <= len(self.expenses): #1에서 최대까지.
+                deleted_expense = self.expenses.pop(choice - 1) #1을 입력하면 리스트에서 0인덱스 삭제해야함.
+                print(f"'{deleted_expense.description}' 지출이 삭제되었습니다.\n")
+            else:
+                print("잘못된 번호입니다. 지출 목록에 있는 번호를 입력해주세요.\n")
+        except ValueError:
+            print("유효하지 않은 입력입니다. 숫자를 입력해주세요.\n")
+        except Exception as e:
+            print(f"오류 발생: {e}\n")
+
+
     def list_expenses(self):
         if not self.expenses:
             print("지출 내역이 없습니다.\n")
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -7,9 +7,10 @@ def main():
     while True:
         print("==== 간단 가계부 ====")
         print("1. 지출 추가")
-        print("2. 지출 목록 보기")
-        print("3. 총 지출 보기")
-        print("4. 종료")
+        print("2. 지출 삭제")
+        print("3. 지출 목록 보기")
+        print("4. 총 지출 보기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -21,17 +22,17 @@ def main():
                 print("잘못된 금액입니다.\n")
                 continue
             budget.add_expense(category, description, amount)
-
         elif choice == "2":
+            budget.delete_expense()
+        elif choice == "3":
             budget.list_expenses()
 
-        elif choice == "3":
+        elif choice == "4":
             budget.total_spent()
 
-        elif choice == "4":
+        elif choice == "5":
             print("가계부를 종료합니다.")
-            break
-
+            break   
         else:
             print("잘못된 선택입니다.\n")
 
