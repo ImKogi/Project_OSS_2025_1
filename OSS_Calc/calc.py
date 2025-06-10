8991451f00f36b56ad5926e22d73809dad68fb52
Koen <gjyj0223@gmail.com>
2025-06-10 06:01:09 -0800

    [기능추가]  백스페이스 키 동작 방식 수정

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import time
 
 class Calculator:
     def __init__(self, root):
@@ -52,12 +52,15 @@ class Calculator:
         #키보드로 입력 받기
         self.root.bind("<Key>", self.on_key_press)
         self.root.bind("<Return>", lambda event: self.on_click('='))
-        self.root.bind("<BackSpace>", lambda event: self.on_click('C'))
+        self.root.bind("<KeyPress-BackSpace>", self.on_backspace_press)
+        self.root.bind("<KeyRelease-BackSpace>", self.on_backspace_release)
+        self.press_time = None
 
-        
     def on_click(self, char):
         if char == 'C':
             self.expression = ""
+        elif char == 'BackSpace':
+            self.expression = self.expression[:-1]
         elif char == '=':
             try:
                 self.expression = str(eval(self.expression))
@@ -73,3 +76,16 @@ class Calculator:
         key = event.char
         if key in "0123456789+-*/.":
             self.on_click(key) # 유효한 숫자 또는 기호(문자)를 계산기 로직으로 전달
+
+    def on_backspace_press(self, event):
+        if self.press_time is None:
+            self.press_time = time.time()
+
+    def on_backspace_release(self, event):
+        if self.press_time is not None:
+            duration = time.time() - self.press_time
+            if duration < 1:  # 1초 미만이면 한 글자 삭제
+                self.on_click('BackSpace')
+            else:  # 1초 이상이면 전체 삭제
+                self.press_time = None
+                self.on_click('C')
